fab378511acb5d4ae25f355aa04ea7be0e86ce50
V Purushotham <purushothamv8500@gmail.com>
2024-12-02 12:23:33 +0530

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,16 @@
+// this is a single line comment
+
+let x = 5;
+
+
+/* this is a multi line comments */
+
+let y = 10;
+
+
+/* addition of two values */
+
+let z = x+y;
+console.log(z);
+
+let a = 100; // inline comment
