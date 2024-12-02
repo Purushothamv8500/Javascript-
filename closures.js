05adfa03ad60f60c9a6f5a8cf755e6bd5e87af25
V Purushotham <purushothamv8500@gmail.com>
2024-12-02 12:23:01 +0530

    Add files via upload

---
+++ closures.js
@@ -0,0 +1,11 @@
+const pet = function(name){
+    
+const getName = function() {
+    return name;
+}
+
+return getName ;
+
+}
+const petname = pet("viva");
+console.log(petname());
---
+++ functions.js
@@ -0,0 +1,14 @@
+/// functions 
+
+function msg(name){
+    return 'hello, ${name}!';
+}
+console.log(msg("purush"));
+
+
+console.log("=====================")
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(3, 4)); // Output: 7
